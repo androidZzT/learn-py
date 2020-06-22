d67c26daa252e642e8396269e20ac95268d0d27c
zhangzhengtian <zhangzhengtian@kuaishou.com>
2020-06-22 15:43:30 +0800

    add fibonacci.py and string.py

---
+++ string.py
@@ -0,0 +1,7 @@
+word = 'Python'
+
+print(word[0])
+print(word[-1]) # last character
+
+print(word[0:2]) # characters from position 0 (included) to 2 (excluded)
+print(word[:9]) # handle index out of bounds, output 'Python'
